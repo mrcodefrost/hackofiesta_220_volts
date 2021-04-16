99f4043bca0bd4459e76f8f7491441db6d048464
mrcodefrost <72447688+mrcodefrost@users.noreply.github.com>
2021-04-17 03:14:08 +0530

    update

---
+++ lib/screens/reading_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ReadingScreen extends StatefulWidget {
+  @override
+  _ReadingScreenState createState() => _ReadingScreenState();
+}
+
+class _ReadingScreenState extends State<ReadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
